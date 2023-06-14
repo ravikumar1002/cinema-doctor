ed4f5fb6e7f3141f01df551a66dd05f0ddab323f
Ravi kumar <1002kumarravi@gmail.com>
2023-06-14 18:28:31 +0530

    api calls

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,36 @@
+import { GetTmdbDataAsJSON } from "@/app/src/services/getApiData";
+import { GetServerSideProps } from "next";
+import MovieCard from "./components/movie-card/MovieCard";
+import axios from "axios";
 
-const Home = () => {
+export const getTrendingData = async () => {
+
+  const trendingResponse = await GetTmdbDataAsJSON<any>("/trending/all/day", {
+    params: {
+      page: "3",
+    },
+  });
+
+  return trendingResponse;
+}
+
+const Home = async () => {
+
+  const trendingData = await getTrendingData();
+
   return (
     <main className="">
-      This is a cinema doctor
+      {
+        trendingData && trendingData.results.map((_, i) => {
+          return (
+            <MovieCard key={trendingData.results[i].id} movieList={trendingData.results[i]} />
+          )
+        })
+      }
+
     </main>
   )
 }
 
 export default Home
+
