5e55e259bd6ab86ad5c91a5dfa8134fca1921d18
Ravi kumar <1002kumarravi@gmail.com>
2023-06-16 16:34:35 +0530

    tmdb api format

--- app/src/services/axiosInstance/tmdb.ts
+++ app/src/services/axiosInstance/tmdb.ts
@@ -1,11 +1,12 @@
-import appConfigs from '../../config/appConfigs';
 import axios from 'axios';
+import appConfigs from '../../config/appConfigs';
 
 const tmdbAxiosInstance = axios.create({
     baseURL: 'https://api.themoviedb.org/3/',
 });
 
 tmdbAxiosInstance.interceptors.request.use((config) => {
+    console.log(config, appConfigs.tmdb.apiKey)
     config.params = {
         ...config.params,
         api_key: appConfigs.tmdb.apiKey
