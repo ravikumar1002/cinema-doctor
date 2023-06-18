cc665f8f7c911ecc7d14fed0e126de7202d9e18d
Ravi kumar <1002kumarravi@gmail.com>
2023-06-18 22:51:23 +0530

    add image route

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "www.themoviedb.org",
+        port: "",
+        pathname: "/t/p/**",
+      },
+    ],
+  },
+};
 
-}
-
-module.exports = nextConfig
+module.exports = nextConfig;
